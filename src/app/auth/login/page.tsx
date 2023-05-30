60ed4e640e9522c4655efda86879780439f973d2
João Lourenção <jotap.lourencao@gmail.com>
2023-05-29 23:18:30 -0300

    feat: first commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { ModalLogin } from '@/components/ModalLogin'
+
+export default function Login() {
+  return (
+    <main className="grid min-h-screen grid-cols-1">
+      <ModalLogin />
+    </main>
+  )
+}
---
+++ src/components/ModalLogin.tsx
@@ -0,0 +1,11 @@
+import { Button, Input } from 'antd'
+
+export function ModalLogin() {
+  return (
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-3xl bg-white p-5 shadow-lg">
+      <Input />
+      <Input />
+      <Button />
+    </div>
+  )
+}
